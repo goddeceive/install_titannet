5ad51e09e111411e56ef755d1568201d5ef7aa02
goddeceive <42476772+goddeceive@users.noreply.github.com>
2024-04-10 20:16:11 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,11 +6,13 @@ sudo apt update
 # 安裝依賴包
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# 添加 Docker 的官方 GPG 密鑰
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# 下載 Docker 的官方 GPG 密鑰並將其加入到 Docker 的 keyring
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/docker-archive-keyring.gpg >/dev/null
 
 # 設置 Docker 的穩定版儲存庫，並自動輸入 Enter 鍵
-echo | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null <<EOF
+deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
+EOF
 
 # 更新 apt 包索引
 sudo apt update
@@ -21,5 +23,5 @@ sudo apt install -y docker-ce
 # 啟動 Docker 服務
 sudo systemctl start docker
 
-# 驗證 Docker 是否安装成功
+# 驗證 Docker 是否安裝成功
 docker --version
