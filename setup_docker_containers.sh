9c6564497714a03db8ab4589759e35c8ec9d9fe0
goddeceive <42476772+goddeceive@users.noreply.github.com>
2024-04-10 20:38:47 +0800

    Update setup_docker_containers.sh

--- setup_docker_containers.sh
+++ setup_docker_containers.sh
@@ -22,6 +22,6 @@ echo "請輸入你的 hash: "
 read -r hash
 
 # 循環進入每个容器，並執行命令
-for i in {2..5}; do
+for i in {1..5}; do
     docker exec -i docker$i bash -c "echo \"$hash\" | titan-edge bind --hash=\$(cat) https://api-test1.container1.titannet.io/api/v2/device/binding"
 done
