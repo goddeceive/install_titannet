45f3161abcf281dd74eb9800d82b5d83786218b7
goddeceive <42476772+goddeceive@users.noreply.github.com>
2024-04-10 20:01:00 +0800

    Update setup_docker_containers.sh

--- setup_docker_containers.sh
+++ setup_docker_containers.sh
@@ -16,3 +16,12 @@ docker run -d -v ~/.titanedge2:/root/.titanedge --name docker2 nezha123/titan-ed
 docker run -d -v ~/.titanedge3:/root/.titanedge --name docker3 nezha123/titan-edge
 docker run -d -v ~/.titanedge4:/root/.titanedge --name docker4 nezha123/titan-edge
 docker run -d -v ~/.titanedge5:/root/.titanedge --name docker5 nezha123/titan-edge
+
+# 步驟 4：提示用户輸入 hash
+echo "請輸入你的 hash: "
+read -r hash
+
+# 循環進入每个容器，並執行命令
+for i in {2..5}; do
+    docker exec -i docker$i bash -c "echo \"$hash\" | titan-edge bind --hash=\$(cat) https://api-test1.container1.titannet.io/api/v2/device/binding"
+done
